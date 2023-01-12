57ad7be26652f278397598bd8de5840090d77276
Italo Reis <italomatiasddosreis@gmail.com>
2023-01-12 20:47:52 -0300

    Finalizado jogo da velha com reflections
    Finalizado jogo da velha com reflections

--- JOGO DA FORCA/src/main/java/jogoforca/core/Dictionary.java
+++ JOGO DA FORCA/src/main/java/jogoforca/core/Dictionary.java
@@ -2,6 +2,8 @@ package jogoforca.core;
 
 import jogoforca.game.GameException;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,6 +14,17 @@ public abstract class Dictionary  {
 
     public static Dictionary getInstance(){
         if (instance == null){
+            String dictionaryClassName = Config.get("dictionayClassName");
+            Class<?> clazz = null;
+            try {
+                clazz = Class.forName(dictionaryClassName);
+                Constructor<?> constructor = clazz.getConstructor();
+                instance = (Dictionary) constructor.newInstance();
+            } catch (Exception e ) {
+                throw  new RuntimeException(e);
+            }
+
+
             instance = new StaticDictionary();
         }
         return instance;
